299739ab665b381d07feb42f4a7e947c220cad15
Steven Rieder <s.rieder@uva.nl>
2026-05-08 11:38:53 +0200

    add name of the worker

--- amuse_seculartriple/interface.py
+++ amuse_seculartriple/interface.py
@@ -46,7 +46,7 @@ class SeculartripleInterface(CodeInterface):
     include_headers = ['src/main_code.h','src/ODE_system.h']
 
     def __init__(self, **options):
-         CodeInterface.__init__(self, **options)
+         CodeInterface.__init__(self, name_of_the_worker='seculartriple_worker', **options)
         
     @legacy_function
     def evolve():
